34499125b51a9054c934d66dc551558c0817a42a
Lilly Rosaline <lilly.rosaline@waterlilly.dev>
2022-01-22 21:10:15 -0600

    Apparently my commit failed. great.

--- src/main/java/online/maestoso/soulforged/client/SoulforgedClient.java
+++ src/main/java/online/maestoso/soulforged/client/SoulforgedClient.java
@@ -1,7 +1,6 @@
 package online.maestoso.soulforged.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import online.maestoso.soulforged.client.render.SoulforgedModelPredicates;
 
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
 public class SoulforgedClient implements ClientModInitializer {
