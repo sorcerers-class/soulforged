7b0b613526108bbe3d54daedbdf5ed33cac56e96
Lilly Rosaline <lilly.rosaline@waterlilly.dev>
2022-01-24 06:46:30 -0600

    partially fix model names

--- src/main/java/online/maestoso/soulforged/client/render/ForgedToolItemModel.java
+++ src/main/java/online/maestoso/soulforged/client/render/ForgedToolItemModel.java
@@ -35,7 +35,9 @@ import net.minecraft.world.BlockRenderView;
 
 import online.maestoso.soulforged.Soulforged;
 
+import online.maestoso.soulforged.item.tool.ForgedToolType;
 import online.maestoso.soulforged.item.tool.ForgedToolTypes;
+import online.maestoso.soulforged.material.SmithingMaterial;
 import online.maestoso.soulforged.material.SmithingMaterials;
 
 import org.jetbrains.annotations.Nullable;
@@ -48,7 +50,11 @@ public class ForgedToolItemModel implements UnbakedModel, BakedModel, FabricBake
     private static final Identifier ITEM_HANDHELD_MODEL = new Identifier("minecraft:item/handheld");
     private static final HashMap<String, BakedModel> PART_MODELS = new HashMap<>();
     private ModelTransformation transformation;
-
+    private enum ModelToolParts {
+        HEAD,
+        BINDING,
+        HANDLE
+    }
     public ForgedToolItemModel() {
     }
     @Environment(EnvType.CLIENT)
@@ -63,18 +69,13 @@ public class ForgedToolItemModel implements UnbakedModel, BakedModel, FabricBake
         String binding_mat = new Identifier(nbt.getCompound("sf_binding").getString("material")).getPath();
         String handle_mat = new Identifier(nbt.getCompound("sf_handle").getString("material")).getPath();
 
-        String head = head_mat + "_" + type + "_" + "head";
-        String binding = binding_mat + "_" + type + "_" + "binding";
-        String handle = handle_mat + "_" + type + "_" + "handle";
+        String head = modelNameConcatenation(head_mat, type, ModelToolParts.HEAD);
+        String binding = modelNameConcatenation(binding_mat, type, ModelToolParts.BINDING);
+        String handle = modelNameConcatenation(handle_mat, type, ModelToolParts.HANDLE);
 
         context.fallbackConsumer().accept(PART_MODELS.get(head));
         context.fallbackConsumer().accept(PART_MODELS.get(binding));
         context.fallbackConsumer().accept(PART_MODELS.get(handle));
-    }
-
-    @Override
-    public Collection<SpriteIdentifier> getTextureDependencies(Function<Identifier, UnbakedModel> unbakedModelGetter, Set<Pair<String, String>> unresolvedTextureReferences) {
-       return Collections.emptyList();
     }
 
     @Nullable
@@ -84,27 +85,35 @@ public class ForgedToolItemModel implements UnbakedModel, BakedModel, FabricBake
         transformation = defaultItemModel.getTransformations();
 
         if(PART_MODELS.isEmpty()) {
-            ForgedToolTypes.TOOL_TYPES_REGISTRY.forEach(raw_type ->
-                    SmithingMaterials.SMITHING_MATERIALS_REGISTRY.forEach(raw_material -> {
-                        String material = Objects.requireNonNull(SmithingMaterials.SMITHING_MATERIALS_REGISTRY.getId(raw_material)).getPath();
-                        if(raw_material.canIntoTool()) {
-                            String type = Objects.requireNonNull(ForgedToolTypes.TOOL_TYPES_REGISTRY.getId(raw_type)).getPath();
-                            PART_MODELS.put(material + "_" + type + "_" + "head", loader.bake(new ModelIdentifier("soulforged:" + material + "_" + type + "_" + "head", "inventory"), ModelRotation.X0_Y0));
-                            PART_MODELS.put(material + "_" + type + "_" + "binding", loader.bake(new ModelIdentifier("soulforged:" + material + "_" + type + "_" + "binding", "inventory"), ModelRotation.X0_Y0));
-                            PART_MODELS.put(material + "_" + type + "_" + "handle", loader.bake(new ModelIdentifier("soulforged:" + material + "_" + type + "_" + "handle", "inventory"), ModelRotation.X0_Y0));
+            ForgedToolTypes.TOOL_TYPES_REGISTRY.forEach(type ->
+                    SmithingMaterials.SMITHING_MATERIALS_REGISTRY.forEach(material -> {
+                        if(material.canIntoTool()) {
+                            PART_MODELS.put(getModelName(material, type, ModelToolParts.HEAD), loader.bake(new ModelIdentifier(new Identifier("soulforged", getModelName(material, type, ModelToolParts.HEAD)), "inventory"), ModelRotation.X0_Y0));
+                            PART_MODELS.put(getModelName(material, type, ModelToolParts.BINDING), loader.bake(new ModelIdentifier(new Identifier("soulforged", getModelName(material, type, ModelToolParts.BINDING)), "inventory"), ModelRotation.X0_Y0));
+                            PART_MODELS.put(getModelName(material, type, ModelToolParts.HANDLE), loader.bake(new ModelIdentifier(new Identifier("soulforged", getModelName(material, type, ModelToolParts.HANDLE)), "inventory"), ModelRotation.X0_Y0));
                         }
                     })
             );
         }
-
         return this;
     }
 
-    @Override
-    public Collection<Identifier> getModelDependencies() {
+    public static String getModelName(SmithingMaterial material, ForgedToolType type, ModelToolParts part) {
+        String stype = Objects.requireNonNull(ForgedToolTypes.TOOL_TYPES_REGISTRY.getId(type)).getPath();
+        String smaterial = Objects.requireNonNull(SmithingMaterials.SMITHING_MATERIALS_REGISTRY.getId(material)).getPath();
+        return modelNameConcatenation(smaterial, stype, part);
+    }
+    public static String modelNameConcatenation(String material, String type, ModelToolParts part) {
+        return material + "_" + type + "_" + part.toString().toLowerCase();
+    }
+
+    // Below is all the stuff I don't care about. It's down here because I don't care about it.
+    @Override public Collection<Identifier> getModelDependencies() {
         return Collections.emptyList();
     }
-    // Below is all the stuff I don't care about. It's down here because I don't care about it.
+    @Override public Collection<SpriteIdentifier> getTextureDependencies(Function<Identifier, UnbakedModel> unbakedModelGetter, Set<Pair<String, String>> unresolvedTextureReferences) {
+        return Collections.emptyList();
+    }
     @Override public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction face, Random random) {
         Soulforged.LOGGER.fatal("Call to ForgedToolItemModel.getQuads");
         return List.of();
@@ -124,7 +133,6 @@ public class ForgedToolItemModel implements UnbakedModel, BakedModel, FabricBake
     @Override public Sprite getParticleSprite() {
         return MinecraftClient.getInstance().getSpriteAtlas(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE).apply(new Identifier("block/cobblestone"));
     }
-
     @Override public ModelTransformation getTransformation() {
         return transformation;
     }
