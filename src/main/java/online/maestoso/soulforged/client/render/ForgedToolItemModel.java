97fc66d742b0b0586ad4bd399b513036f6d41084
Lilly Rosaline <lilly.rosaline@waterlilly.dev>
2022-01-24 23:18:06 -0600

    before branch

--- src/main/java/online/maestoso/soulforged/client/render/ForgedToolItemModel.java
+++ src/main/java/online/maestoso/soulforged/client/render/ForgedToolItemModel.java
@@ -55,8 +55,7 @@ public class ForgedToolItemModel implements UnbakedModel, BakedModel, FabricBake
         BINDING,
         HANDLE
     }
-    public ForgedToolItemModel() {
-    }
+    public ForgedToolItemModel() {}
     @Environment(EnvType.CLIENT)
     @Override
     public void emitItemQuads(ItemStack stack, Supplier<Random> randomSupplier, RenderContext context) {
@@ -83,14 +82,13 @@ public class ForgedToolItemModel implements UnbakedModel, BakedModel, FabricBake
     public BakedModel bake(ModelLoader loader, Function<SpriteIdentifier, Sprite> textureGetter, ModelBakeSettings rotationContainer, Identifier modelId) {
         JsonUnbakedModel defaultItemModel = (JsonUnbakedModel) loader.getOrLoadModel(ITEM_HANDHELD_MODEL);
         transformation = defaultItemModel.getTransformations();
-        defaultItemModel.bake(loader, textureGetter, rotationContainer, modelId);
         if(PART_MODELS.isEmpty()) {
             ForgedToolTypes.TOOL_TYPES_REGISTRY.forEach(type ->
                     SmithingMaterials.SMITHING_MATERIALS_REGISTRY.forEach(material -> {
                         if(material.canIntoTool()) {
-                            PART_MODELS.put(getModelName(material, type, ModelToolParts.HEAD), loader.bake(new ModelIdentifier(new Identifier("soulforged", getModelName(material, type, ModelToolParts.HEAD)), "inventory"), ModelRotation.X0_Y0));
-                            PART_MODELS.put(getModelName(material, type, ModelToolParts.BINDING), loader.bake(new ModelIdentifier(new Identifier("soulforged", getModelName(material, type, ModelToolParts.BINDING)), "inventory"), ModelRotation.X0_Y0));
-                            PART_MODELS.put(getModelName(material, type, ModelToolParts.HANDLE), loader.bake(new ModelIdentifier(new Identifier("soulforged", getModelName(material, type, ModelToolParts.HANDLE)), "inventory"), ModelRotation.X0_Y0));
+                            PART_MODELS.put(getModelName(material, type, ModelToolParts.HEAD), loader.bake(new ModelIdentifier(new Identifier("soulforged", getModelName(material, type, ModelToolParts.HEAD)), "inventory"), rotationContainer));
+                            PART_MODELS.put(getModelName(material, type, ModelToolParts.BINDING), loader.bake(new ModelIdentifier(new Identifier("soulforged", getModelName(material, type, ModelToolParts.BINDING)), "inventory"), rotationContainer));
+                            PART_MODELS.put(getModelName(material, type, ModelToolParts.HANDLE), loader.bake(new ModelIdentifier(new Identifier("soulforged", getModelName(material, type, ModelToolParts.HANDLE)), "inventory"), rotationContainer));
                         }
                     })
             );
