75cc0d6b6bd716e66762324cc99e35da44d812ab
Lilly Rosaline <lilly.rosaline@waterlilly.dev>
2022-04-09 15:28:37 -0500

    added names to ToolParts

--- src/main/kotlin/studio/soulforged/soulforged/item/tool/part/ToolPart.kt
+++ src/main/kotlin/studio/soulforged/soulforged/item/tool/part/ToolPart.kt
@@ -1,3 +1,3 @@
 package studio.soulforged.soulforged.item.tool.part
 
-class ToolPart(val weight: Double, val durability: Double)
+class ToolPart(val name: String, val weight: Double, val durability: Double)
