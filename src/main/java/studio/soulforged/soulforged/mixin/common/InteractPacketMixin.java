ae2c2c04f53464b8589303b518cb2630c27f5192
Lilly Rosaline <lilly.rosaline@waterlilly.dev>
2023-03-29 17:15:59 -0500

    Release 0.3.0 (finally!)

--- src/main/java/studio/soulforged/soulforged/mixin/common/InteractPacketMixin.java
+++ src/main/java/studio/soulforged/soulforged/mixin/common/InteractPacketMixin.java
@@ -1,6 +1,6 @@
 package studio.soulforged.soulforged.mixin.common;
 
-import net.minecraft.network.packet.c2s.play.PlayerInteractionWithEntityC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,8 +15,8 @@ import studio.soulforged.soulforged.item.SoulforgedItems;
 public class InteractPacketMixin {
     @Shadow
     public ServerPlayerEntity player;
-    @Inject(method = "onPlayerInteractionWithEntity(Lnet/minecraft/network/packet/c2s/play/PlayerInteractionWithEntityC2SPacket;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/c2s/play/PlayerInteractionWithEntityC2SPacket;getEntity(Lnet/minecraft/server/world/ServerWorld;)Lnet/minecraft/entity/Entity;"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
-    private void soulforged$injectOnPlayerInteractionWithEntity(PlayerInteractionWithEntityC2SPacket packet, CallbackInfo ci) {
+    @Inject(method = "onPlayerInteractEntity(Lnet/minecraft/network/packet/c2s/play/PlayerInteractEntityC2SPacket;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/c2s/play/PlayerInteractEntityC2SPacket;getEntity(Lnet/minecraft/server/world/ServerWorld;)Lnet/minecraft/entity/Entity;"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
+    private void soulforged$injectOnPlayerInteractionWithEntity(PlayerInteractEntityC2SPacket packet, CallbackInfo ci) {
         if(this.player != null) {
             if(this.player.getMainHandStack() != null) {
                 if(this.player.getMainHandStack().isOf(SoulforgedItems.INSTANCE.getTOOL())) {
